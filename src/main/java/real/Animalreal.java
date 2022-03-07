a79259aa9bea016110d6e07d1df464cd16d53f4c
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-03-07 23:50:43 +0300

    add the db

---
+++ src/main/java/real/Animalreal.java
@@ -0,0 +1,12 @@
+package real;
+
+import models.Animal;
+
+import java.util.List;
+
+public interface Animalreal{
+
+    void add(Animal animals);
+    List<Animal> getAll();
+
+}
---
+++ src/main/java/real/Sightingreal.java
@@ -0,0 +1,11 @@
+package real;
+
+import models.Sighting;
+
+import java.util.List;
+
+public interface Sightingreal {
+    void add(Sighting sighting);
+    List<Sighting> getAll();
+
+}
