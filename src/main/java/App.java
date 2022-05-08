54d6cf166838446c21987d6006cb6614093b7025
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-05-08 17:22:00 +0300

    create the abstract class of Animals and implement it

--- src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-public class App {
-    public static void main(String[] args) {
-get("/",(request,response)->{
-
-}),new HandlebarsTemplateEngine());
-    }
-}
---
+++ src/main/java/dao/AnimalDao.java
@@ -0,0 +1,9 @@
+package dao;
+import models.Animals;
+
+import java.util.List;
+
+public interface AnimalDao {
+    void add(Animals animals);
+    List<Animals> getAll();
+}
